3a3d2f042501290ac5243a7b7688576271e59dfe
DanDanTheMuffinMan <dshafton888@gmail.com>
2026-03-08 19:38:26 -0500

    WEFWEF

---
+++ PiPifier iOS/PiPifier iOS-Prefix.swift
@@ -0,0 +1,4 @@
+//  Converted to Swift 6.2 by Swiftify v6.2.23088 - https://swiftify.com/
+//
+// Prefix header for all source files of the `PiPifier iOS` target in the 'PiPifier iOS' project.
+//
---
+++ macOS/PiPifier-Prefix.swift
@@ -0,0 +1,4 @@
+//  Converted to Swift 6.2 by Swiftify v6.2.23088 - https://swiftify.com/
+//
+// Prefix header for all source files of the `PiPifier` target in the 'PiPifier macOS' project.
+//
